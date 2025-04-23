51eb105b1966ce8708db6641385f548522fa38e7
urzoey77 <urzoey77@gmail.com>
2025-04-23 14:57:50 -0400

    完成TopBar样式

---
+++ mi-shop/src/TopBar/TopBar.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./TopBar.css";
+import "../../public/iconfont/iconfont.css";
+
+const TopBar = () => {
+  return (
+    <div className="topBarWrap">
+      <div className="siteNav">
+        <a href="javascript:">小米官网</a>
+        <a href="javascript:">小米商城</a>
+        <a href="javascript:">MIUI</a>
+        <a href="javascript:">IoT</a>
+        <a href="javascript:">云服务</a>
+        <a href="javascript:">天星数科</a>
+        <a href="javascript:">有品</a>
+        <a href="javascript:">小爱开放平台</a>
+        <a href="javascript:">企业团购</a>
+        <a href="javascript:">资质证照</a>
+        <a href="javascript:">协议规则</a>
+        <a href="javascript:">下载app</a>
+        <a href="javascript:">Select Location</a>
+      </div>
+      <div className="userNav">
+        <a href="javascript:">登录</a>
+        <a href="javascript:">注册</a>
+        <a href="javascript:">消息通知</a>
+        <div className="shoppingCar">
+          <a href="javascript:" className="iconfont icon-gouwuchekong">
+            购物车
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
